20e14e89e6aba97fb4f12eb87e6b7c0647851541
chpoit <chpoit@chpoit.com>
2022-10-30 13:16:46 -0400

    woopsie

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-// File destination.txt will be created or overwritten by default.
-fs.copyFile('source.txt', 'destination.txt', (err) => {
-    if (err) throw err;
-    console.log('source.txt was copied to destination.txt');
-});
-
 try {
     // `who-to-greet` input defined in action metadata file
     const sourceFile = core.getInput('source');
