bfd946098b991962e993cfa06e597454d5d9269a
chpoit <chpoit@chpoit.com>
2022-10-30 13:22:27 -0400

    testing new package

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
+const copy = require('copy');
 
 try {
     // `who-to-greet` input defined in action metadata file
     const sourceFile = core.getInput('source');
     const targetFile = core.getInput('target');
     console.log(`Copying "${sourceFile}" to "${targetFile}`);
-    fs.copyFileSync(sourceFile, targetFile);
+
+    // fs.copyFileSync(sourceFile, targetFile);
+
+    copy(sourceFile, targetFile, (err, files) => {
+        if (err) {
+            core.setFailed(err);
+        }
+    });
 } catch (error) {
     core.setFailed(error.message);
 }
