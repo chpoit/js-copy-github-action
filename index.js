13ee28a2fe5cfc1e5a46cc80dcd567d4acf61ce9
Charles Poitras <chpoit@chpoit.com>
2022-11-03 08:49:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const github = require('@actions/github');
 const copy = require('copy');
 
 try {
-    // `who-to-greet` input defined in action metadata file
     const sourceFile = core.getInput('source');
     const targetFile = core.getInput('target');
     console.log(`Copying "${sourceFile}" to "${targetFile}"`);
-
-    // fs.copyFileSync(sourceFile, targetFile);
 
     copy(sourceFile, targetFile, (err, files) => {
         if (err) {
